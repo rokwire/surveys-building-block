05de0a40880a017779e66bdd87295c56a663ef7c
akshadpai <akshadpai01@gmail.com>
2023-06-08 10:56:10 -0500

    Edit createSurvey to send notifications

--- core/app_shared.go
+++ core/app_shared.go
@@ -16,6 +16,7 @@ package core
 
 import (
 	"application/core/model"
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
@@ -30,15 +31,94 @@ func (a appShared) getSurvey(id string, orgID string, appID string) (*model.Surv
 	return a.app.storage.GetSurvey(id, orgID, appID)
 }
 
-func (a appShared) getSurveys(orgID string, appID string, surveyIDs []string, surveyTypes []string, limit *int, offset *int) ([]model.Survey, error) {
+func (a appShared) getSurveys(orgID string, appID string, surveyIDs []string, surveyTypes []string, limit *int, offset *int, groupID string) ([]model.Survey, error) {
 	return a.app.storage.GetSurveys(orgID, appID, surveyIDs, surveyTypes, limit, offset)
 }
 
-func (a appShared) createSurvey(survey model.Survey) (*model.Survey, error) {
+func (a appShared) createSurvey(survey model.Survey, user model.User) (*model.Survey, error) {
 	survey.ID = uuid.NewString()
 	survey.DateCreated = time.Now().UTC()
 	survey.DateUpdated = nil
+
+	if len(survey.UserIDs) > 0 {
+		sendNotificationsToUserList(a, survey, user)
+	} else if len(survey.GroupIDs) > 0 {
+		for _, groupID := range survey.GroupIDs {
+			sendNotificationsToGroup(a, survey, user, groupID)
+		}
+	}
+
 	return a.app.storage.CreateSurvey(survey)
+}
+
+func sendNotificationsToUserList(a appShared, survey model.Survey, user model.User) {
+	messageRecipients := make([]model.NotificationMessageRecipient, len(survey.UserIDs))
+	for i, userID := range survey.UserIDs {
+		messageRecipients[i] = model.NotificationMessageRecipient{
+			UserID: userID,
+			// TODO: check mute
+			Mute: false,
+		}
+	}
+
+	a.app.notifications.SendNotification(model.NotificationMessage{
+		OrgID: survey.OrgID,
+		AppID: survey.AppID,
+		Recipients: messageRecipients,
+		Sender: &model.Sender{
+			Type: "user",
+			User: &model.UserRef{
+				UserID: user.Claims.Subject,
+				Name:   user.Claims.Name,
+			},
+		},
+		Topic:   "survey",
+		Subject: "Illinois",
+		Body:    fmt.Sprintf("Survey '%s' has been created", survey.Title),
+		Data: map[string]string{
+			"type":        survey.Type,
+			"operation":   "survey_created",
+			"entity_type": "survey",
+			"entity_id":   survey.ID.Hex(),
+			"entity_name": survey.Title,
+		},
+	})
+}
+
+func sendNotificationsToGroup(a appShared, survey model.Survey, user model.User, groupID string) {
+	members, err := a.app.groups.GetGroupMembers(user.Token, groupID)
+	if err != nil {
+		return
+	}
+	messageRecipients := make([]model.UserRef, len(*members))
+	for i, member := range *members {
+		messageRecipients[i] = model.UserRef{
+			UserID: member.ClientID,
+			Name: member.Name,
+		}
+	}
+
+	a.app.groups.SendGroupNotification(groupID, model.GroupNotification {
+		Members: messageRecipients,
+		Sender: &model.Sender{
+			Type: "user",
+			User: &model.UserRef{
+				UserID: user.Claims.Subject,
+				Name:   user.Claims.Name,
+			},
+		},
+		Topic:   "survey",
+		Subject: "Illinois",
+		Body:    fmt.Sprintf("Survey '%s' has been created", survey.Title),
+		Data: map[string]string{
+			"group_id":    groupID,
+			"type":        "survey",
+			"operation":   "survey_created",
+			"entity_type": "survey",
+			"entity_id":   survey.ID.Hex(),
+			"entity_name": survey.Title,
+		},
+	})
 }
 
 func (a appShared) updateSurvey(survey model.Survey, admin bool) error {
