9bfd7353f7f5670538e890a5b41492f96d6ef135
akshadpai <akshadpai01@gmail.com>
2023-07-26 14:48:14 -0500

    Update setCachedConfigs

--- driven/storage/adapter.go
+++ driven/storage/adapter.go
@@ -100,7 +100,7 @@ func (a *Adapter) setCachedConfigs(configs []model.Config) {
 		case model.ConfigTypeEnv:
 			err = parseConfigsData[model.EnvConfigData](&config)
 		case model.ConfigTypeAuth:
-			err = parseConfigsData[model.AuthConfigData](&config)
+			err = parseConfigsData[model.EnvConfigData](&config)
 		default:
 			err = parseConfigsData[map[string]interface{}](&config)
 		}
